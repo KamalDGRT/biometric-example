86ff7f7b007111ba9847ea9144ef79496f7c2aad
KamalDGRT <kamaldgrt@gmail.com>
2025-04-13 20:21:48 +0530

    initial app setup

---
+++ BiometricAuthExample/BiometricAuthExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiometricAuthExampleApp.swift
+//  BiometricAuthExample
+//
+//  Created by Kamal Sharma on 13/04/25.
+//
+
+import SwiftUI
+
+@main
+struct BiometricAuthExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
